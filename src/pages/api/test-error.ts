720f5187d643fee9742cf5c1f7a7567ee3c0beee
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-01-22 07:33:55 +0000

    refine api test endpoint

--- src/pages/api/test-error.ts
+++ src/pages/api/test-error.ts
@@ -1,5 +1,8 @@
+import type { APIRoute } from "astro";
+
 export const prerender = false;
 
-export async function GET() {
+export const GET: APIRoute = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   throw new Error("Sentry Example API Route Error");
-}
+};
